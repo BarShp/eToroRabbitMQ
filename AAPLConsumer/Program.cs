af5245a46ca2b49c91ac737d2e19534e5b89884c
MitziTheGoat <Bar.shpigler@gmail.com>
2019-07-12 15:48:34 +0300

    Added AAPL and AllStocks consumers

---
+++ AAPLConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using BasicRabbitMQ;
+
+namespace AAPLConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfigurationRoot configuration = builder.Build();
+
+            using (var consumer =
+                new BasicTopicConsumer(
+                    configuration["RabbitMQUrl"],
+                    configuration["QueueName"],
+                    configuration["ExchangeName"],
+                    configuration["BindingKey"]))
+            {
+                consumer.Consume((c, msg) =>
+                {
+                    Console.WriteLine(msg);
+                });
+
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
