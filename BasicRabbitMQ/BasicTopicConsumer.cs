9612193860c1b1f19e2cf1b2f868bf4d11149778
MitziTheGoat <Bar.shpigler@gmail.com>
2019-07-12 15:32:25 +0300

    Created basic consumer and producer

---
+++ BasicRabbitMQ/BasicTopicConsumer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace BasicRabbitMQ
+{
+    public class BasicTopicConsumer : IDisposable
+    {
+        readonly string queueName;
+        IConnection connection;
+        IModel channel;
+
+        public BasicTopicConsumer(string rabbitMQUrl, string queueName, string exchangeName, string bindingKey)
+        {
+            this.queueName = queueName;
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri(rabbitMQUrl)
+            };
+            this.connection = factory.CreateConnection();
+            this.channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchangeName, "topic", autoDelete: false);
+            channel.QueueDeclare(queueName, exclusive: false, autoDelete: false);
+            channel.QueueBind(queueName, exchangeName, bindingKey);
+        }
+
+        public void Consume(EventHandler<string> onReceived)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                onReceived(this, message);
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+        }
+
+        public void Dispose()
+        {
+            connection.Dispose();
+            channel.Dispose();
+        }
+    }
+}
---
+++ BasicRabbitMQ/BasicTopicProducer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace BasicRabbitMQ
+{
+    public class BasicTopicProducer : IDisposable
+    {
+        readonly string exchangeName;
+        IConnection connection;
+        IModel channel;
+
+        public BasicTopicProducer(string rabbitMQUrl, string exchangeName)
+        {
+            this.exchangeName = exchangeName;
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri(rabbitMQUrl)
+            };
+            this.connection = factory.CreateConnection();
+            this.channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchangeName, "topic", autoDelete: false);
+        }
+
+        public void PublishMessage(string message, string routingKey)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: exchangeName,
+                                 routingKey: routingKey,
+                                 body: body);
+        }
+
+        public void Dispose()
+        {
+            connection.Dispose();
+            channel.Dispose();
+        }
+    }
+}
