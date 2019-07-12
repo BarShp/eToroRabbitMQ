45ce0e47ad716b880ce52b0a4bc9505bacff373a
MitziTheGoat <Bar.shpigler@gmail.com>
2019-07-12 15:34:51 +0300

    Created GOOG consumer

---
+++ GOOGConsumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using BasicRabbitMQ;
+
+namespace GOOGConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfigurationRoot configuration = builder.Build();
+
+            using (var consumer =
+                new BasicTopicConsumer(
+                    configuration["RabbitMQUrl"],
+                    configuration["QueueName"],
+                    configuration["ExchangeName"],
+                    configuration["BindingKey"]))
+            {
+                consumer.Consume((c, msg) =>
+                {
+                    Console.WriteLine(msg);
+                });
+
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
