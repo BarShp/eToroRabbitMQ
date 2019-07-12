de2988a7fbab06a09ff540f36d95ed86d8eca77f
MitziTheGoat <Bar.shpigler@gmail.com>
2019-07-12 16:04:40 +0300

    Created a common for the common StockInfo model

---
+++ Common/Models/StockInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common
+{
+    public class StockInfo
+    {
+        public string ID { get; set; }
+        public double BuyRate { get; set; }
+        public string SellRate { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
