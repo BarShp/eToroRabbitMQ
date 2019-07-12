06b666fd72cbdd1ed164df192fa6e2db52644e2c
MitziTheGoat <Bar.shpigler@gmail.com>
2019-07-12 15:34:23 +0300

    Created Stocks Publisher (Currently working by just running it)

---
+++ StocksPublisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using BasicRabbitMQ;
+using Microsoft.Extensions.Configuration;
+
+namespace StocksPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+
+            IConfigurationRoot configuration = builder.Build();
+
+            using (var bp = new BasicTopicProducer(configuration["RabbitMQUrl"], "MyExchange"))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    bp.PublishMessage("Hello World!", "badRoutingKey");
+                    bp.PublishMessage("Hello World!", "goodRoutingKey");
+                }
+            }
+        }
+    }
+}
